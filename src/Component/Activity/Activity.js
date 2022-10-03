9c24505e47cde310ea2110c889a9d418d1a4a4b9
salsabeelnoor <salsabeelnoor@gmail.com>
2022-10-04 01:49:29 +0600

    data fetch from json done

--- src/Component/Activity/Activity.js
+++ src/Component/Activity/Activity.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Detail from '../Detail/Detail';
 import Exercise from '../Exercise/Exercise';
 import './Activity.css'
 
 const Activity = () => {
+    const [exercises, setExercise] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setExercise(data));
+    }, []);
     return (
         <div className='grid lg:grid-cols-layout grid-cols-mobile_layout'>
             <div className=''>
                 <h1 className='pt-20 text-center lg:text-left lg:pl-12 text-2xl font-semibold'>Select Today's Exercise</h1>
-                <Exercise></Exercise>
+                <div>
+                    {
+                        exercises.map(exercise => <Exercise
+                        key={exercise.id}
+                        exercise={exercise}
+                        ></Exercise>)
+                    }
+                </div>
             </div>
             <div className='bg-pink-300 min-h-screen'>
                <Detail></Detail>
--- src/Component/Exercise/Exercise.js
+++ src/Component/Exercise/Exercise.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const Exercise = () => {
+const Exercise = ({exercise}) => {
+    const {name, time, information, image} = exercise;
     return (
-        <div className='border-4'>
-            <h1>This is exercise</h1>
+        <div className='pl-12 pt-10 grid grid-cols-3'>
             <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md ">
                 <a href="#">
-                <img class="rounded-t-lg" src="" alt="" />
+                <img class="rounded-t-lg" src={image} alt="..." />
                 </a>
                 <div class="p-5">
-                    <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{name}</h1>
+                    <p>Time: {time}s</p>
+                    <p class="mb-3 font-normal text-gray-700 ">{information}</p>
                     <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                     Read more
                     </a>
