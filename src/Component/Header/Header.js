f4ef124bc20b1207d6fda6c16e84396136219aca
salsabeelnoor <salsabeelnoor@gmail.com>
2022-10-03 23:07:10 +0600

    nav completed

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className=' bg-rose-300'>
+            <nav className='container mx-auto pt-4 pb-4'>
+                <h1 className='text-left text-2xl font-bold text-pink-900'>Activity <span className='text-rose-400'>Booster</span> Club</h1>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
